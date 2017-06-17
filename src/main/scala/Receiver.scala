d992c79f712aebbf8f277e6a87935cd21c186603
Marcin Przewięźlikowski <m.przewie@gmail.com>
2017-06-17 12:16:45 +0200

    Nicer way of loading clients from config file

--- src/main/scala/Receiver.scala
+++ src/main/scala/Receiver.scala
@@ -2,18 +2,23 @@
   * Created by marcin on 5/6/17.
   */
 
-import java.util
-
 import scala.collection.JavaConverters._
 import akka.actor._
 import communication.Node
-import com.typesafe.config.ConfigFactory
+import com.typesafe.config.{Config, ConfigFactory}
 import communication.Request
 
-import scalaj.http.{Http, HttpOptions}
 
 
 object Main extends App {
+
+  def refPathFromConfig(config: Config): String = {
+    val host = config.getString("host")
+    val port = config.getInt("port").toString
+    val systemName = config.getString("systemName")
+    val controllerName = config.getString("controllerName")
+    s"akka.tcp://$systemName@$host:$port/user/$controllerName"
+  }
 
   def ping(url: String): Unit = {
     system.actorSelection(url).tell(Request(nodeName), cloudia)
@@ -22,7 +27,7 @@ object Main extends App {
 
   def nodeName: String = ConfigFactory.load().getString("cloudia.nodeName")
 
-  def clients: List[String] = ConfigFactory.load().getStringList("cloudia.clients").asScala.toList
+  def clients: List[Config] = ConfigFactory.load().getConfigList("cloudia.clients").asScala.toList
 
   def systemName: String = ConfigFactory.load().getString("cloudia.systemName")
 
@@ -32,13 +37,15 @@ object Main extends App {
   val system = ActorSystem(systemName)
   val cloudia = system.actorOf(Node.props(directory), name = nodeName)
   println("started")
-  clients.filter(_.startsWith("akka.tcp://")).foreach(ping)
+  clients.map(refPathFromConfig).foreach(ping)
 
   do {
     println("Press Enter to ping all clients listed in application.conf")
     println("You can also enter additional url you want  to ping")
     val newUrl = scala.io.StdIn.readLine()
-    (newUrl :: clients).filter(_.startsWith("akka.tcp://")).foreach(ping)
+    (newUrl :: clients.map(refPathFromConfig)).filter(_.startsWith("akka.tcp://")).foreach(ping)
 
   } while (true)
+
+
 }
